65276db4272d7d34814c92c6e657f0dbcce6f1fc
Benedict Mendoza <biller010@gmail.com>
2018-03-13 16:04:51 -0600

    fix merge issues

--- src/Files/Driver.java
+++ src/Files/Driver.java
@@ -8,27 +8,24 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
-<<<<<<< Updated upstream
 		DirectoryParser parser = new DirectoryParser(args[0]);
-=======
-		DirectoryParser parser = new DirectoryParser("C:\\Users\\ben\\OneDrive\\Documents\\SENG-300\\Assignments\\testDirectory\\src\\testProject");
->>>>>>> Stashed changes
 		CompilationUnit[] asts = parser.parseDirectory();
 		
 		//System.out.println(asts[0].toString());
 		//System.out.println(asts[3].toString());
-		Counter stringCounter = new Counter("java.lang.String");
+		QualifiedNameCounter counter = new QualifiedNameCounter(args[1]);
 		//asts[0].accept(counter);
 		//DeclarationCounter counter2 = new DeclarationCounter("java.lang.String");
-
-		asts[0].accept(stringCounter);
-		asts[1].accept(stringCounter);
+		for (int i = 0; parse.getLength(); i++) { //ADD parse.getLenth() to DirectoryParser()
+			asts[i].accept(counter);
+		}
 		//System.out.println(asts[0].toString());
 		//System.out.println(asts[0].propertyDescriptors(AST.JLS8).get(2));
 	//	System.out.println(asts[0].TYPES_PROPERTY);
 		//System.out.println(asts[0].getNodeType() == ASTNode.COMPILATION_UNIT);
 	//	System.out.println(asts[0].properties());
-		System.out.println("declaration count " + stringCounter.getDeclarationCount());
-		System.out.println("reference count " + stringCounter.getReferenceCount());
+		//System.out.println("declaration count " + stringCounter.getDeclarationCount());
+		//System.out.println("reference count " + stringCounter.getReferenceCount());
+		//System.out.println(arg0);
 	}
 }
