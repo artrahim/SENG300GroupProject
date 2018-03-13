f87bbecb81783fa6ad52cb72e8227b2e7c93a649
Benedict Mendoza <biller010@gmail.com>
2018-03-13 15:21:46 -0600

    my changes to driver

--- src/Files/Driver.java
+++ src/Files/Driver.java
@@ -8,16 +8,27 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
+<<<<<<< Updated upstream
 		DirectoryParser parser = new DirectoryParser(args[0]);
+=======
+		DirectoryParser parser = new DirectoryParser("C:\\Users\\ben\\OneDrive\\Documents\\SENG-300\\Assignments\\testDirectory\\src\\testProject");
+>>>>>>> Stashed changes
 		CompilationUnit[] asts = parser.parseDirectory();
-		System.out.println(asts[0].toString());
-		System.out.println(asts[1].toString());
-		asts[0].accept(new DeclarationCounter("String"));
+		
+		//System.out.println(asts[0].toString());
+		//System.out.println(asts[3].toString());
+		Counter stringCounter = new Counter("java.lang.String");
+		//asts[0].accept(counter);
+		//DeclarationCounter counter2 = new DeclarationCounter("java.lang.String");
+
+		asts[0].accept(stringCounter);
+		asts[1].accept(stringCounter);
 		//System.out.println(asts[0].toString());
 		//System.out.println(asts[0].propertyDescriptors(AST.JLS8).get(2));
-		System.out.println(asts[0].TYPES_PROPERTY);
-		System.out.println(asts[0].getNodeType() == ASTNode.COMPILATION_UNIT);
-		System.out.println(asts[0].properties());
-		System.out.println("it worked?");
+	//	System.out.println(asts[0].TYPES_PROPERTY);
+		//System.out.println(asts[0].getNodeType() == ASTNode.COMPILATION_UNIT);
+	//	System.out.println(asts[0].properties());
+		System.out.println("declaration count " + stringCounter.getDeclarationCount());
+		System.out.println("reference count " + stringCounter.getReferenceCount());
 	}
 }
