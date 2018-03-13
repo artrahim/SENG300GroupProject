d7e58e649d42aafd03ca27c11239cb9a80d2c68d
Benedict Mendoza <biller010@gmail.com>
2018-03-12 19:40:15 -0600

    class I use to run the program (produces the documentation bug)

---
+++ src/Files/Driver.java
@@ -0,0 +1,23 @@
+package Files;
+
+import java.io.FileNotFoundException;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+public class Driver {
+	public static void main(String[] args) throws FileNotFoundException {
+		DirectoryParser parser = new DirectoryParser("C:\\Users\\bille\\OneDrive\\Documents\\SENG-300\\Assignments\\testDirectory\\src\\testProject");
+		CompilationUnit[] asts = parser.parseDirectory();
+		System.out.println(asts[0].toString());
+		System.out.println(asts[1].toString());
+		asts[0].accept(new DeclarationCounter("String"));
+		//System.out.println(asts[0].toString());
+		//System.out.println(asts[0].propertyDescriptors(AST.JLS8).get(2));
+		System.out.println(asts[0].TYPES_PROPERTY);
+		System.out.println(asts[0].getNodeType() == ASTNode.COMPILATION_UNIT);
+		System.out.println(asts[0].properties());
+		System.out.println("it worked?");
+	}
+}
