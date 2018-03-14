2722911a1ae7f9f8c59f8675927cd67f85599b50
Benedict Mendoza <biller010@gmail.com>
2018-03-14 00:01:44 -0600

    driver accounts for when args == 0 and prints a message

--- src/Files/Driver.java
+++ src/Files/Driver.java
@@ -10,18 +10,28 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 // calls the appropriate functions to run the program
 public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
-		// initialize the parser to the given directory
-		DirectoryParser parser = new DirectoryParser(args[0]);
-		CompilationUnit[] asts = parser.parseDirectory();
-		// initialize the counter to the given name
-		QualifiedNameCounter counter = new QualifiedNameCounter(args[1]);
-
-		// visits the nodes for each ast
-		for (int i = 0; i < Array.getLength(asts) ; i++) { //ADD parse.getLenth() to DirectoryParser()
-			asts[i].accept(counter);
-		}
+		// initialize variables
+		DirectoryParser parser;
+		CompilationUnit[] asts;
+		QualifiedNameCounter counter;
 		
-		// prints the result
-		System.out.println(args[1] + "." + " Declarations found: " + counter.getDeclarationCount() + ";" + " references found: " + counter.getReferenceCount());
+		// initialize the parser to the given directory
+		if (Array.getLength(args) != 0) {
+			parser = new DirectoryParser(args[0]);
+			asts = parser.parseDirectory();
+			// initialize the counter to the given name
+			counter = new QualifiedNameCounter(args[1]);
+
+			// visits the nodes for each ast
+			for (int i = 0; i < Array.getLength(asts) ; i++) { //ADD parse.getLenth() to DirectoryParser()
+				asts[i].accept(counter);
+			}
+			
+			// prints the result
+			System.out.println(args[1] + "." + " Declarations found: " + counter.getDeclarationCount() + ";" + " references found: " + counter.getReferenceCount());
+
+		} else {
+			System.out.println("Please input: java Driver <directory of java files> <java type to search> ");
+		}
 	}
 }
