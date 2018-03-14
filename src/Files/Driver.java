d7524c21c2442c4ccb1104c348073f7843b4374d
dominic-demierre <aneroidluxation@gmail.com>
2018-03-14 00:07:40 -0600

    Driver checks whether AST list is null

--- src/Files/Driver.java
+++ src/Files/Driver.java
@@ -3,8 +3,6 @@ package Files;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 // calls the appropriate functions to run the program
@@ -16,20 +14,22 @@ public class Driver {
 		QualifiedNameCounter counter;
 		
 		// initialize the parser to the given directory
-		if (Array.getLength(args) != 0) {
+		if (Array.getLength(args) == 2) {
 			parser = new DirectoryParser(args[0]);
 			asts = parser.parseDirectory();
-			// initialize the counter to the given name
-			counter = new QualifiedNameCounter(args[1]);
-
-			// visits the nodes for each ast
-			for (int i = 0; i < Array.getLength(asts) ; i++) { //ADD parse.getLenth() to DirectoryParser()
-				asts[i].accept(counter);
-			}
 			
-			// prints the result
-			System.out.println(args[1] + "." + " Declarations found: " + counter.getDeclarationCount() + ";" + " references found: " + counter.getReferenceCount());
-
+			if (asts != null) {
+				// initialize the counter to the given name
+				counter = new QualifiedNameCounter(args[1]);
+	
+				// visits the nodes for each ast
+				for (int i = 0; i < Array.getLength(asts) ; i++) {
+					asts[i].accept(counter);
+				}
+				
+				// prints the result
+				System.out.println(args[1] + "." + " Declarations found: " + counter.getDeclarationCount() + ";" + " references found: " + counter.getReferenceCount());
+			}
 		} else {
 			System.out.println("Please input: java Driver <directory of java files> <java type to search> ");
 		}
